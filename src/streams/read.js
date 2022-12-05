8c4a36d94f2cf6310c7b4a086e2f1439143b914a
Ilona <mitsunaki@yandex.ru>
2022-12-05 19:50:10 +0300

    feat: make streams, wt, zip, cp tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+	const path = join(__dirname, 'files', 'fileToRead.txt');
+	let readStream = createReadStream(path);
+	let data = '';
+	readStream.on('data', chunk => (data += chunk));
+	readStream.on('end', () => process.stdout._write(data));
 };
 
 await read();
